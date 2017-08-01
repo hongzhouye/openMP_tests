764923b555d88a713a5a27724a99ee7ab710b4b1
hongzhouye <hzye2011@mit.edu>
2017-08-01 11:34:17 -0400

    Small modifications of prog-3.cpp

--- prog-3.cpp
+++ prog-3.cpp
@@ -7,16 +7,21 @@ int main()
 {
     const int size = 20000;
     std::vector<double> a(size, 0.);
+    int x = -1;
 
     double start = omp_get_wtime();
-#pragma omp parallel for
-        for(size_t i = 0; i < size; i++)
-        {
-            a[i] = 0.;
-            for(size_t j = 0; j < size; j++)
-                a[i] += sqrt(sqrt(i + j));
-        }
+#pragma omp parallel    // create threads
+{
+    x = omp_get_thread_num() + 100;
+#pragma omp for
+    for(size_t i = 0; i < size; i++)
+    {
+        for(size_t j = 0; j < size; j++)
+            a[i] += sqrt(sqrt(i + j));
+    }
+}
     double stop = omp_get_wtime();
+    std::cout << "x = " << x << std::endl;
     std::cout << "wall time: " << stop - start << " sec" << std::endl;
 
     return 0;
